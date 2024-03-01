fd34a918d151115d211fc2b6ae836da59777e707
MariskaPutri18 <mariskaputri960@gmail.com>
2024-03-01 20:58:30 +0700

    modif luas kel

--- PersegiPanjang18.java
+++ PersegiPanjang18.java
@@ -6,4 +6,17 @@ class PersegiPanjang18 {
         public void cetakInfo() {
             System.out.println("Panjang: " + panjang + ", Lebar: " + lebar);
         }
+
+        public PersegiPanjang18(int p, int l){
+            panjang = p;
+            lebar = l;
+        }
+
+        public int hitungLuas(){
+            return panjang * lebar;
+        }
+        
+        public int hitungKeliling(){
+            return 2*(panjang + lebar);
+        }
 }
